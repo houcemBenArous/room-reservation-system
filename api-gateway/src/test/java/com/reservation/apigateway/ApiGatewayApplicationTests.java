3087cef7997a8f877101ad62b957b0db551aa22e
houcemBenArous <houcem.benarous2001@gmail.com>
2026-01-02 17:35:27 +0100

    Initial commit: Room Reservation System microservices

---
+++ api-gateway/src/test/java/com/reservation/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ auth-service/src/test/java/com/reservation/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
